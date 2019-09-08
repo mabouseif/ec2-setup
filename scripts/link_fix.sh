339c753cf11bd2600d6b175a8d85eae9c8556137
mabouseif <moabo18@student.sdu.dk>
2019-09-08 14:45:14 +0200

    Added the script for fixing the linking problems

---
+++ scripts/link_fix.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+sudo rm /usr/lib/x86_64-linux-gnu/libGL.so
+sudo ln -s /usr/lib/x86_64-linux-gnu/libGL.so.1.7.0 /usr/lib/x86_64-linux-gnu/libGL.so
+
+
+sudo ln -s /usr/lib/python2.7/dist-packages/vtk/libvtkRenderingPythonTkWidgets.x86_64-linux-gnu.so /usr/lib/x86_64-linux-gnu/libvtkRenderingPythonTkWidgets.so
+
+sudo rm /usr/lib/x86_64-linux-gnu/libEGL.so
+sudo ln /usr/lib/x86_64-linux-gnu/libEGL.so.1.1.0 /usr/lib/x86_64-linux-gnu/libEGL.so
+
